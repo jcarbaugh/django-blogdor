03497c618b5f349bbe9294cadfae4bcd1269ca24
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-06-04 09:19:41 -0400

    add enable/disable comments to admin

--- blogdor/admin.py
+++ blogdor/admin.py
@@ -10,25 +10,33 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     exclude = ('date_published','is_published',)
     search_fields = ('title','content')
-    actions = ('publish_posts','recall_posts')
+    actions = ('publish_posts','recall_posts','enable_comments','disable_comments')
     
     def publish_posts(self, request, queryset):
         """
         Mark select posts as published and set date_published if it does not exist.
         """
         now = datetime.datetime.now()
-        published_count = queryset.publish()
-        s = published_count == 1 and 'posts' or 'post'
-        self.message_user(request, "%i %s published" % (published_count, s))
+        count = queryset.publish()
+        self.message_user(request, "%i post(s) published" % count)
     publish_posts.short_description = "Publish posts"
     
     def recall_posts(self, request, queryset):
         """
         Recall published posts, but leave date_published as is.
         """
-        recalled_count = queryset.recall()
-        s = recalled_count == 1 and 'posts' or 'post'
-        self.message_user(request, "%i %s recalled" % (recalled_count, s))
+        count = queryset.recall()
+        self.message_user(request, "%i post(s) recalled" % count)
     recall_posts.short_description = "Recall published posts"
+    
+    def enable_comments(self, request, queryset):
+        count = queryset.update(comments_enabled=True)
+        self.message_user(request, "Comments enabled on %i post(s)" % count)
+    enable_comments.short_description = "Enable comments"
+
+    def disable_comments(self, request, queryset):
+        count = queryset.update(comments_enabled=False)
+        self.message_user(request, "Comments disabled on %i post(s)" % count)
+    disable_comments.short_description = "Disable comments"
 
 admin.site.register(Post, PostAdmin)
