19d8676a929bedbc3663aaa9b1f34f4dc51605dc
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-06-19 09:13:59 -0400

    create propert setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+long_description = open('README.markdown').read()
+ 
+setup(
+    name='django-blogdor',
+    version="2.0.0",
+    packages=['blogdor','blogdor.templatetags'],
+    package_dir={'blogdor': 'blogdor'},
+    package_data={'blogdor': ['templates/blogdor/*.html',
+                              'templates/blogdor/feeds/*.html',
+                              'templates/comments/*.html']},
+    description='Django blogging application',
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@gmail.com',
+    license='BSD License',
+    url='https://github.com/sunlightlabs/django-blogdor/',
+    long_description=long_description,
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
