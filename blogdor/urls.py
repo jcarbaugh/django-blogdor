57b9fd196e1c337e6c0f27acf8d6f8deff89776d
Sets88 <sets88@mail.ru>
2013-10-08 14:35:26 +0400

    fix function views to class-based views for django 1.5+

--- blogdor/urls.py
+++ blogdor/urls.py
@@ -1,5 +1,13 @@
 from django.conf import settings
-from django.conf.urls import *
+from django.conf.urls import patterns, include, url
+from blogdor.views import ShowPost
+from blogdor.views import ShowPostWP
+from blogdor.views import ShowYearArchive
+from blogdor.views import ShowMonthArchive
+from blogdor.views import ShowArchive
+from blogdor.views import AuthorPosts
+from blogdor.views import PreviewPost
+from blogdor.views import TagList
 
 urlpatterns = patterns('blogdor.views',
 
@@ -8,22 +16,22 @@ urlpatterns = patterns('blogdor.views',
 
     # tags
     url(r'^tag/(?P<tag>[^/]+)/$', 'tag', name='blogdor_tag'),
-    url(r'^tag/$', 'tag_list', name='blogdor_tag_list'),
+    url(r'^tag/$', TagList.as_view(), name='blogdor_tag_list'),
 
     # archives
-    url(r'^(?P<year>\d{4})/$', 'archive_year', name='blogdor_archive_year'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', name='blogdor_archive_month'),
-    url(r'^$', 'archive', name='blogdor_archive'),
+    url(r'^(?P<year>\d{4})/$', ShowYearArchive.as_view(), name='blogdor_archive_year'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', ShowMonthArchive.as_view(), name='blogdor_archive_month'),
+    url(r'^$', ShowArchive.as_view(), name='blogdor_archive'),
 
     # post
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)/$', 'post_wpcompat', name='blogdor_post_wpcompat'),
-    url(r'^(?P<year>\d{4})/(?P<slug>[\w-]+)/$', 'post', name='blogdor_post'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)/$', ShowPostWP.as_view(), name='blogdor_post_wpcompat'),
+    url(r'^(?P<year>\d{4})/(?P<slug>[\w-]+)/$', ShowPost.as_view(), name='blogdor_post'),
 
     # author
-    url(r'^author/(?P<username>[\w]+)/$', 'author', name='blogdor_author'),
+    url(r'^author/(?P<username>[\w]+)/$', AuthorPosts.as_view(), name='blogdor_author'),
 
     # preview
-    url(r'^preview/(?P<post_id>\d+)/(?P<slug>[\w-]+)/$', 'preview', name='blogdor_preview'),
+    url(r'^preview/(?P<post_id>\d+)/(?P<slug>[\w-]+)/$', PreviewPost.as_view(), name='blogdor_preview'),
 
 )
 
@@ -34,6 +42,6 @@ if getattr(settings, 'BLOGDOR_ENABLE_FEEDS', True):
     urlpatterns += patterns('',
         url(r'^feeds/latest/$', feeds.LatestPosts()),
         url(r'^feeds/comments/$', feeds.LatestComments()),
-        url(r'^feeds/tag/$', feeds.LatestForTag()),
-        url(r'^feeds/author/$', feeds.LatestForAuthor()),
+        url(r'^feeds/tag/(?P<tag>[\w-]+)/$', feeds.LatestForTag()),
+        url(r'^feeds/author/(?P<author>[\w-]+)/$', feeds.LatestForAuthor()),
     )
